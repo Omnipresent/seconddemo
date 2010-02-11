82c9d4ca106c71ea5f71515b129f5b89e276ec4b
Bhaarat <bhaarat.s@gmail.com>
2010-02-11 12:46:35 -0500

    new deploy

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
 		belongs_to :customer, :foreign_key => "cid"
 		
-validates_presence_of :line1,:line2,:city
+validates_presence_of :line1,:line2
 validates_numericality_of :zip
 end
